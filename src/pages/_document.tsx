5ba31b695eaa17ab045d57a585daf5a8a52a252d
Alexis <alexis@hecfeuille.fr>
2021-11-09 23:22:42 +0100

    add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,19 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta name="description" content="Alexis Hecfeuille's homepage, full stack web developer based in France." />
+          <meta name="author" content="Alexis Hecfeuille" />
+          <meta name="robots" content="index, follow" />
+          <meta name="keywords" content="hecfeuille,alexis,software,developer,web,developer,front,back,full,stack,javascript,react,node,next,sse,se" />
+          <meta property="og:title" content="Alexis Hecfeuille" />
+          <meta property="og:type" content="website" />
+          <meta property="og:description" content="Alexis Hecfeuille" />
+          <meta property="og:url" content="https://alexis.hecfeuille.fr/" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:locale:alternate" content="fr_FR" />
+          <meta property="og:site_name" content="Alexis Hecfeuille" />
+        </Head>
         <body className="min-h-screen">
           <Main />
           <NextScript />
