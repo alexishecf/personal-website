f19cefb5ba8766b84c545389c33ca934aa5ac516
Alexis <alexis@hecfeuille.fr>
2022-09-06 22:47:21 +0200

    update resume tsx and content

--- lang/fr-FR.ts
+++ lang/fr-FR.ts
@@ -1,119 +1,126 @@
 export const messagesFr = {
   // Header
-  header_home: "Accueil",
-  header_blog: "Blog",
-  header_contact: "Contact",
+  header_home: 'Accueil',
+  header_blog: 'Blog',
+  header_contact: 'Contact',
 
   // Index
-  index_title: "Alexis Hecfeuille | Accueil",
-  index_heroBio: "Je suis un développeur web full-stack situé en France",
-  index_contactMe: "Contactez moi",
+  index_title: 'Alexis Hecfeuille | Accueil',
+  index_heroBio: 'Je suis un développeur web full-stack situé en France',
+  index_contactMe: 'Contactez moi',
   index_about:
     "Je suis un développeur basé en France et passionné d'informatique. Je travaille principalement sur des projets web en tant que développeur full-stack.",
-  index_aboutLabel: "À propos",
-  index_thingsIveWorkedOn: "Projets",
-  index_contact: "Contact",
-  index_technicalSkills: "Compétences techniques",
-  index_softSkills: "Compétences générales",
+  index_aboutLabel: 'À propos',
+  index_thingsIveWorkedOn: 'Projets',
+  index_contact: 'Contact',
+  index_technicalSkills: 'Compétences techniques',
+  index_softSkills: 'Compétences générales',
 
   // About
-  about_softSkill1: "Résolution de problèmes",
-  about_softSkill2: "Adaptabilité",
-  about_softSkill3: "Créativité",
-  about_softSkill4: "Souci du détail",
+  about_softSkill1: 'Résolution de problèmes',
+  about_softSkill2: 'Adaptabilité',
+  about_softSkill3: 'Créativité',
+  about_softSkill4: 'Souci du détail',
 
   // Projects
-  projects_project2: "Site personnel",
-  projects_project1OneLiner: "Arènes en réalité virtuelle et déplacement libre",
-  projects_project2OneLiner: "Site personnel, portfolio et blog",
+  projects_project2: 'Site personnel',
+  projects_project1OneLiner: 'Arènes en réalité virtuelle et déplacement libre',
+  projects_project2OneLiner: 'Site personnel, portfolio et blog',
 
   // Contact
-  contact_paragraph:
-    "Vous pouvez me contacter par mail {mail} ou via {linkedInMessage}.",
+  contact_paragraph: 'Vous pouvez me contacter par mail {mail} ou via {linkedInMessage}.',
 
   // Resume:About
-  resume_title: "Alexis Hecfeuille | CV",
-  resume_jobTitle: "Développeur web",
-  resume_skillsLabel: "Compétences",
+  resume_title: 'Alexis Hecfeuille | CV',
+  resume_jobTitle: 'Développeur web',
+  resume_skillsLabel: 'Compétences',
 
   // Resume:Education
-  resume_educationLabel: "Formation",
-  resume_education2Diploma: "Master, Informatique",
-  resume_education2Courses:
-    "Cours: Algorithmique et Complexité, Architecture et conception des systèmes d'exploitation, Programmation orientée objet, Systèmes multi-agents, Vision par ordinateur, Programmation 3D (OpenGL / C++ / WebGL / JS), Réalité Virtuelle (Unity), Interaction Homme–Machine, Systèmes embarqués (FPGA / VHDL), Calcul distribué.  ",
-  resume_education2University: "Université de Lille",
-  resume_education2Paragraph: "",
-  resume_education1Diploma: "Licence, Informatique",
-  resume_education1Courses:
-    "Cours: Automates et langages, Conception orientée objet, Programmation système, Algorithmique et recherche opérationnelle, Base de données relationnelle, Programmation fonctionnelle, Réseaux, Logique, Algèbre linéaire, Algorithmes, Cryptographie, Technologies du web, Structures de données, Probabilités et statistiques, Mathématiques discrètes, Analyse (mathématiques).",
-  resume_education1University: "Université de Lille",
-  resume_education1Paragraph: "",
+  resume_educationLabel: 'Formation',
+  resume_education2Diploma: 'Master, Informatique',
+  resume_education2Courses: 'Spécialité <i>Image, Vision, Interaction</i> – Mention Bien',
+  resume_education2University: 'Université de Lille',
+  resume_education2Paragraph: '',
+  resume_education1Diploma: 'Licence, Informatique',
+  resume_education1Courses: 'Mention Assez Bien',
+  resume_education1University: 'Université de Lille',
+  resume_education1Paragraph: '',
 
   // Resume:Work Experience
-  resume_workExperienceLabel: "Expérience",
+  resume_workExperienceLabel: 'Expérience',
   resume_workExperience1CompanyDesc:
-    "Start-up technologique développant et vendant des arènes en réalité virtuelle, multijoueur et déplacement libre à des centres de loisirs",
-  resume_workExperience1JobTitle: "Co-fondateur technique",
-  resume_workExperience1Date: "Sep 2018 - Oct 2021",
+    'Start-up technologique développant et vendant des arènes en réalité virtuelle, multijoueur et déplacement libre à des centres de loisirs.',
+  resume_workExperience1JobTitle: 'Co-fondateur technique',
+  resume_workExperience1Date: 'Sep 2018 - Dec 2021',
   resume_workExperience1UnorderedListEl1:
-    "Conception du shooter VR en déplacement libre et multijoueur à destination de casques VR autonomes en utilisant Unity / C#",
+    'Conception du shooter VR en déplacement libre et multijoueur à destination de casques VR autonomes en utilisant Unity / C#.',
   resume_workExperience1UnorderedListEl1_1:
-    "Optimisé pour fonctionner de façon fluide avec jusqu'à 8 joueurs et 40+ PNJs simultanément",
+    "Optimisé pour fonctionner de façon fluide avec jusqu'à 8 joueurs et 40+ PNJs simultanément.",
   resume_workExperience1UnorderedListEl1_2:
-    "Jouable sur une surface physique de 300+ m² et sur une surface virtuelle de 1+ km²",
+    'Jouable sur une surface physique de 300+ m² et sur une surface virtuelle de 1+ km².',
   resume_workExperience1UnorderedListEl2:
-    "Création de l'application de gestion en .NET/C# utilisée par nos clients pour lancer et monitorer les parties",
+    "Création de l'application de gestion en C# et Windows Forms utilisée par nos clients pour lancer et monitorer les parties.",
   resume_workExperience1UnorderedListEl3:
-    "Développement de l'application web d'authentification pour les utilisateurs finaux avec React et TailwindCSS",
+    "Développement de l'application web d'authentification pour les utilisateurs finaux avec React et CodeIgniter.",
   resume_workExperience1UnorderedListEl3_1:
-    "Permet l'authentification des utilisateurs finaux sur des tablettes sans intervention de l'opérateur, réduisant le temps de préparation des parties",
+    "Permet l'authentification des utilisateurs finaux sur des tablettes sans intervention de l'opérateur, réduisant le temps de préparation des parties.",
   resume_workExperience1UnorderedListEl3_2:
-    "Réduit la charge logistique en automatisant l'association joueur—casque et en suivant l'état de configuration de chaque joueur",
+    "Réduit la charge logistique en automatisant l'association joueur—casque et en suivant l'état de configuration de chaque joueur.",
   resume_workExperience1UnorderedListEl4:
-    "Création d'une application web pour analyser les données commerciales et opérationnelles avec Next.js et MySQL",
+    "Création d'une application web pour analyser les données commerciales et opérationnelles avec React/CodeIgniter/MySQL.",
   resume_workExperience1UnorderedListEl5:
-    "Implémentation d'une API REST avec PHP/CodeIgniter pour rendre possible la communication entre les différents éléments de la solution (Casques VR, Application de gestion, Authentification...)",
+    "Implémentation d'une API REST en PHP pour rendre possible la communication entre les différents éléments de la solution (Casques VR, Application de gestion, Authentification...).",
   resume_workExperience1UnorderedListEl6:
-    "Rencontres avec des entrepreneurs du loisir pour faire des démonstrations techniques de la solution dans le but de la vendre",
+    'Rencontres avec des entrepreneurs du loisir pour faire des démonstrations techniques de la solution dans le but de la vendre.',
   resume_workExperience1UnorderedListEl7:
-    "Présentation de la solution dans différents salons pour obtenir des retours de la part d'utilisateurs finaux et pour rencontrer des clients potentiels",
+    "Présentation de la solution dans différents salons pour obtenir des retours de la part d'utilisateurs finaux et pour rencontrer des clients potentiels.",
+
+  resume_workExperience2CompanyDesc:
+    'Arianee développe des solutions web3 de bout-en-bout pour les marques, principalement des solutions pour les jetons non fongibles (NFT).',
+  resume_workExperience2JobTitle: 'Développeur Full Stack',
+  resume_workExperience2Date: "Jan 2022 - Aujourd'hui",
+  resume_workExperience2UnorderedListEl1: `Développement d'un système pour distribuer les NFTs d'Arianee à grande échelle à la fois sur le front-end (Angular) et le back-end (Node/Express/MongoDB) pour répondre aux besoins d'une distribution quasi simultanée à environ 20 000 personnes (stade).`,
+  resume_workExperience2UnorderedListEl2: `Conception et implémentation des deux côtés de l'application (front/back) d'une plateforme d'échange de NFTs Arianee en utilisant Angular,
+  Node, TypeScript, MongoDB, GCP et Solidity. Responsable des stratégies de test et de la mise en production.`,
+
+  resume_workExperience2UnorderedListEl3: `Amélioration du processus de création de <i>lounges</i> de spkz.io (application de chat web3 open-source initiée par Arianee) en développant un
+  validateur de <i>lounge</i> et diverses stratégies (plus des tests) utilisées par les <i>lounges</i>.`,
 
   // Resume:Languages
-  resume_languagesLabel: "Langues",
-  resume_language1: "Français",
-  resume_language1Proficiency: "natif",
-  resume_language2: "Anglais",
-  resume_language2Proficiency: "courant{br}(TOEIC 960)",
-  resume_language3: "Espagnol",
-  resume_language3Proficiency: "notions",
+  resume_languagesLabel: 'Langues',
+  resume_language1: 'Français',
+  resume_language1Proficiency: 'natif',
+  resume_language2: 'Anglais',
+  resume_language2Proficiency: 'courant{br}(TOEIC 960)',
+  resume_language3: 'Espagnol',
+  resume_language3Proficiency: 'notions',
 
   // Footer
-  footer_paragraph:
-    "© Copyright {year} Alexis Hecfeuille. Tous droits réservés.",
-  footer_legal: "Mentions légales",
+  footer_paragraph: '© Copyright {year} Alexis Hecfeuille. Tous droits réservés.',
+  footer_legal: 'Mentions légales',
 
   // Blog
-  blog_title: "Alexis Hecfeuille | Blog",
-  blog_blogLabel: "Blog",
-  blog_paragraph: "Aucun article pour le moment.",
+  blog_title: 'Alexis Hecfeuille | Blog',
+  blog_blogLabel: 'Blog',
+  blog_paragraph: 'Aucun article pour le moment.',
 
   // Legal
-  legal_title: "Alexis Hecfeuille | Mentions légales",
-  legal_legalLabel: "Mentions légales",
-  legal_identity: "Identité",
-  legal_hosting: "Hébergement",
-  legal_information: "Informations",
-  legal_interactivity: "Interactivité",
-  legal_ip: "Propriété intellectuelle",
-  legal_links: "Liens",
+  legal_title: 'Alexis Hecfeuille | Mentions légales',
+  legal_legalLabel: 'Mentions légales',
+  legal_identity: 'Identité',
+  legal_hosting: 'Hébergement',
+  legal_information: 'Informations',
+  legal_interactivity: 'Interactivité',
+  legal_ip: 'Propriété intellectuelle',
+  legal_links: 'Liens',
 
   // error 404
-  e404_title: "Alexis Hecfeuille | Erreur 404",
-  e404_label: "Erreur 404",
+  e404_title: 'Alexis Hecfeuille | Erreur 404',
+  e404_label: 'Erreur 404',
   e404_paragraph: "Cette page n'existe pas ou plus.",
 
   // error 500
-  e500_title: "Alexis Hecfeuille | Erreur 500",
-  e500_label: "Erreur 500",
-  e500_paragraph: "Erreur interne, veuillez réessayer plus tard.",
+  e500_title: 'Alexis Hecfeuille | Erreur 500',
+  e500_label: 'Erreur 500',
+  e500_paragraph: 'Erreur interne, veuillez réessayer plus tard.',
 };
--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,21 +1,17 @@
-import Head from "next/head";
-import Link from "next/link";
-import { enforceMode } from "../lib/darkMode";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import Head from 'next/head';
+import Link from 'next/link';
+import { enforceMode } from '../lib/darkMode';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
-import {
-  faFilePdf,
-  faChevronLeft,
-  faFlag,
-} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faFilePdf, faChevronLeft, faFlag } from '@fortawesome/free-solid-svg-icons';
 
-import { FormattedMessage, FormattedNumber, useIntl } from "react-intl";
+import { FormattedMessage, FormattedNumber, useIntl } from 'react-intl';
 
 const nestedListStyle = {
-  listStyleType: "circle",
+  listStyleType: 'circle',
 };
 
 export default function Resume() {
@@ -29,55 +25,34 @@ export default function Resume() {
   return (
     <div>
       <Head>
-        <title>{intl.formatMessage({ id: "resume_title" })}</title>
+        <title>{intl.formatMessage({ id: 'resume_title' })}</title>
       </Head>
 
       <div className="relative bg-white">
         <div className="max-w-6xl mx-auto px-4 h-14 my-4 border-b print:hidden">
           <Link href="/">
-            <a
-              className="float-left bg-gray-600 py-2 px-4 rounded-md hover:bg-gray-500 text-white font-bold"
-              href="/"
-            >
+            <a className="float-left bg-gray-600 py-2 px-4 rounded-md hover:bg-gray-500 text-white font-bold" href="/">
               <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
             </a>
           </Link>
 
           <div className="float-left py-2">
-            <FontAwesomeIcon
-              icon={faFlag}
-              className="ml-4 text-gray-800"
-            ></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faFlag} className="ml-4 text-gray-800"></FontAwesomeIcon>
             <Link href="/resume" locale="en-US">
-              <a
-                className={`${
-                  router.locale === "en-US" ? "font-bold" : ""
-                } px-2 text-gray-800`}
-                href="/"
-              >
+              <a className={`${router.locale === 'en-US' ? 'font-bold' : ''} px-2 text-gray-800`} href="/">
                 ENG
               </a>
             </Link>
             <span className="">|</span>
             <Link href="/resume" locale="fr">
-              <a
-                className={`${
-                  router.locale === "fr" ? "font-bold" : ""
-                } px-2 text-gray-800`}
-                href="/"
-              >
+              <a className={`${router.locale === 'fr' ? 'font-bold' : ''} px-2 text-gray-800`} href="/">
                 FRA
               </a>
             </Link>
           </div>
           <a
             className="float-right bg-indigo-500 py-2 px-4 rounded-md hover:bg-indigo-400 text-white font-bold"
-            href={
-              router.locale === "fr"
-                ? "/files/hecfeuille_resume_fr.pdf"
-                : "/files/hecfeuille_resume.pdf"
-            }
-          >
+            href={router.locale === 'fr' ? '/files/hecfeuille_resume_fr.pdf' : '/files/hecfeuille_resume.pdf'}>
             <FontAwesomeIcon icon={faFilePdf}></FontAwesomeIcon> PDF
           </a>
         </div>
@@ -90,31 +65,23 @@ export default function Resume() {
             </div>
 
             <div className="inline-block ml-6 mt-7">
-              <h1 className="text-3xl font-semibold text-gray-900 tracking-wide">
-                ALEXIS HECFEUILLE
-              </h1>
+              <h1 className="text-3xl font-semibold text-gray-900 tracking-wide">ALEXIS HECFEUILLE</h1>
               <h2 className="mt-3 tracking-wider">
                 <FormattedMessage id="resume_jobTitle" />
               </h2>
               <p className="text-gray-600 text-sm mt-1 tracking-wider">
-                Berck, France |{" "}
+                Berck, France |{' '}
                 <a href="https://alexis.hecfeuille.fr/" target="_blank">
                   alexis.hecfeuille.fr
-                </a>{" "}
-                | <a href="mailto:alexis@hecfeuille.fr">alexis@hecfeuille.fr</a>{" "}
-                | +33 6 51 19 29 38
+                </a>{' '}
+                | <a href="mailto:alexis@hecfeuille.fr">alexis@hecfeuille.fr</a> | +33 6 51 19 29 38
                 <br />
                 <a href="https://github.com/alexishecf" target="_blank">
                   <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> alexishecf
-                </a>{" "}
+                </a>{' '}
                 /
-                <a
-                  href="https://linkedin.com/in/alexishecfeuille"
-                  target="_blank"
-                  className="ml-1"
-                >
-                  <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>{" "}
-                  alexishecfeuille
+                <a href="https://linkedin.com/in/alexishecfeuille" target="_blank" className="ml-1">
+                  <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon> alexishecfeuille
                 </a>
               </p>
             </div>
@@ -127,72 +94,50 @@ export default function Resume() {
 
               <div>
                 <ul>
-                  {[
-                    "HTML",
-                    "CSS",
-                    "PHP",
-                    "JavaScript",
-                    "SQL",
-                    "GraphQL",
-                    "React",
-                    "Next.js",
-                    "Node.js",
-                    "MongoDB",
-                    "MySQL",
-                  ].map((skill, index) => (
+                  <li
+                    className={` text-gray-400  mt-1 w-24
+                      } text-sm  inline-block`}>
+                    Languages
+                  </li>
+                  {['TypeScript', 'JS', 'C#', 'SQL', 'Python', 'Solidity'].map((skill, index) => (
                     <li
                       key={index}
                       className={`border text-indigo-500 px-4 mr-1 mt-1
-                      } py-1 text-sm border-indigo-500 inline-block`}
-                    >
+                      } text-sm border-indigo-200 inline-block`}>
                       {skill}
                     </li>
                   ))}
                 </ul>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="ml-8 lg:ml-28 mt-10 print:ml-10">
-              <h1 className="text-md uppercase text-gray-400 tracking-wider  my-6">
-                <FormattedMessage id="resume_educationLabel" />
-              </h1>
-
-              <div className="education-list">
-                <div className="education-item">
-                  <a
-                    href="https://sciences-technologies.univ-lille.fr/informatique/"
-                    target="_blank"
-                  >
-                    <h2 className="mt-3 tracking-wider text-gray-900 uppercase text-lg font-semibold">
-                      <FormattedMessage id="resume_education1University" />
-                    </h2>
-                  </a>
-                  <h3 className="text-gray-600 text-sm tracking-wider mt-1">
-                    <FormattedMessage id="resume_education2Diploma" /> | 2019 –
-                    2021
-                  </h3>
-                  <p className="mt-2 text-sm tracking-wide text-gray-900">
-                    <FormattedMessage id="resume_education2Courses" />
-                  </p>
-                </div>
-                <div className="education-item mt-8">
-                  <a
-                    href="https://sciences-technologies.univ-lille.fr/informatique/"
-                    target="_blank"
-                  >
-                    <h2 className="mt-3 tracking-wider text-gray-900 uppercase text-lg font-semibold">
-                      <FormattedMessage id="resume_education1University" />
-                    </h2>
-                  </a>
-                  <h3 className="text-gray-600 text-sm tracking-wider mt-1">
-                    <FormattedMessage id="resume_education1Diploma" /> | 2016 –
-                    2019
-                  </h3>
-                  <p className="mt-2 text-sm tracking-wide text-gray-900">
-                    <FormattedMessage id="resume_education1Courses" />
-                  </p>
-                </div>
+                <ul>
+                  <li
+                    className={` text-gray-400 w-24 mt-1
+                      }  text-sm  inline-block`}>
+                    Outils
+                  </li>
+                  {['VS Code', 'Git', 'Docker', 'Postman'].map((skill, index) => (
+                    <li
+                      key={index}
+                      className={`border text-indigo-500 px-4 mr-1 mt-1
+                      }  text-sm border-indigo-200 inline-block`}>
+                      {skill}
+                    </li>
+                  ))}
+                </ul>
+                <ul>
+                  <li
+                    className={` text-gray-400 w-24 mt-1
+                      } text-sm  inline-block`}>
+                    Technologies
+                  </li>
+                  {['Angular', 'Node', 'React', 'PostgreSQL', 'MongoDB', 'Web3', 'Unity'].map((skill, index) => (
+                    <li
+                      key={index}
+                      className={`border text-indigo-500 px-4 mr-1 mt-1
+                      }  text-sm border-indigo-200 inline-block`}>
+                      {skill}
+                    </li>
+                  ))}
+                </ul>
               </div>
             </div>
           </div>
@@ -205,18 +150,43 @@ export default function Resume() {
 
               <div className="experience-list">
                 <div className="experience-item">
+                  <a href="https://arianee.org/" target="_blank">
+                    <h2 className="mt-3 tracking-wider text-gray-900 uppercase text-lg font-semibold">Arianee</h2>
+                  </a>
+                  <h3 className="mt-1 text-sm text-gray-900 tracking-wider">
+                    <FormattedMessage id="resume_workExperience2CompanyDesc" />
+                  </h3>
+
+                  <h4 className="text-gray-500 text-sm tracking-wider mt-2">
+                    <FormattedMessage id="resume_workExperience2JobTitle" /> | Paris, France | Jan 2022 – Aujourd'hui
+                  </h4>
+                  <ul className="list-disc ml-5 mt-3 text-gray-900 tracking-wider text-sm ">
+                    <li className="py-1">
+                      <FormattedMessage id="resume_workExperience2UnorderedListEl1" />
+                    </li>
+                    <li className="pt-1">
+                      <FormattedMessage id="resume_workExperience2UnorderedListEl2" />
+                    </li>
+                    <li className="pt-1">
+                      <p>
+                        Amélioration du processus de création de <i>lounge</i> de spkz.io (application de chat web3
+                        open-source initiée par Arianee) en développant un validateur de <i>lounge</i> et diverses
+                        stratégies (plus des tests) utilisées par les <i>lounges</i>.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="experience-item mt-8">
                   <a href="https://hologame.fr/" target="_blank">
-                    <h2 className="mt-3 tracking-wider text-gray-900 uppercase text-lg font-semibold">
-                      hologame
-                    </h2>
+                    <h2 className="mt-3 tracking-wider text-gray-900 uppercase text-lg font-semibold">hologame</h2>
                   </a>
                   <h3 className="mt-1 text-sm text-gray-900 tracking-wider">
                     <FormattedMessage id="resume_workExperience1CompanyDesc" />
                   </h3>
 
                   <h4 className="text-gray-500 text-sm tracking-wider mt-2">
-                    <FormattedMessage id="resume_workExperience1JobTitle" /> |
-                    Tourcoing, France | Sep 2018 – Oct 2021
+                    <FormattedMessage id="resume_workExperience1JobTitle" /> | Tourcoing, France | Sep 2018 – Dec 2021
                   </h4>
                   <ul className="list-disc ml-5 mt-3 text-gray-900 tracking-wider text-sm ">
                     <li className="py-1">
@@ -237,9 +207,6 @@ export default function Resume() {
                       <FormattedMessage id="resume_workExperience1UnorderedListEl3" />
                       <ul className="list-disc ml-5" style={nestedListStyle}>
                         <li className="pt-1">
-                          <FormattedMessage id="resume_workExperience1UnorderedListEl3_1" />
-                        </li>
-                        <li className="pt-1">
                           <FormattedMessage id="resume_workExperience1UnorderedListEl3_2" />
                         </li>
                       </ul>
@@ -250,9 +217,6 @@ export default function Resume() {
                     <li className="py-1">
                       <FormattedMessage id="resume_workExperience1UnorderedListEl5" />
                     </li>
-                    <li className="py-1">
-                      <FormattedMessage id="resume_workExperience1UnorderedListEl6" />
-                    </li>
                   </ul>
                 </div>
               </div>
@@ -260,7 +224,44 @@ export default function Resume() {
           </div>
 
           <div>
-            <div className="ml-8 lg:ml-28 mt-10 pb-28 print:pb-0 print:ml-10">
+            <div className="ml-8 lg:ml-28 mt-10 print:ml-10">
+              <h1 className="text-md uppercase text-gray-400 tracking-wider  my-6">
+                <FormattedMessage id="resume_educationLabel" />
+              </h1>
+
+              <div className="education-list">
+                <div className="education-item">
+                  <a href="https://sciences-technologies.univ-lille.fr/informatique/" target="_blank">
+                    <h2 className="mt-3 tracking-wider text-gray-900 uppercase text-lg font-semibold">
+                      <FormattedMessage id="resume_education1University" />
+                    </h2>
+                  </a>
+                  <h3 className="text-gray-600 text-sm tracking-wider mt-1">
+                    <FormattedMessage id="resume_education2Diploma" /> | 2019 – 2021
+                  </h3>
+                  <p className="mt-2 text-sm tracking-wide text-gray-900">
+                    Spécialité <i>Image, Vision, Interaction</i> – Mention Bien
+                  </p>
+                </div>
+                <div className="education-item mt-8">
+                  <a href="https://sciences-technologies.univ-lille.fr/informatique/" target="_blank">
+                    <h2 className="mt-3 tracking-wider text-gray-900 uppercase text-lg font-semibold">
+                      <FormattedMessage id="resume_education1University" />
+                    </h2>
+                  </a>
+                  <h3 className="text-gray-600 text-sm tracking-wider mt-1">
+                    <FormattedMessage id="resume_education1Diploma" /> | 2016 – 2019
+                  </h3>
+                  <p className="mt-2 text-sm tracking-wide text-gray-900">
+                    <FormattedMessage id="resume_education1Courses" />
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="ml-8 lg:ml-28 mt-8 pb-28 print:pb-0 print:ml-10">
               <h1 className="text-md uppercase text-gray-400 tracking-wider mt-4 mb-6">
                 <FormattedMessage id="resume_languagesLabel" />
               </h1>
